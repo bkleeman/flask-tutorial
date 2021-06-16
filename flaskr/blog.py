635d584335c1b8f66efd3edd43f6b032b78eba5d
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-06-16 16:46:21 -0500

    add create form

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, url_for
 )
+from flask.globals import request
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
@@ -17,3 +18,26 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            "Title is required."
+        
+        if error is not None:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('/blog/create.html')
