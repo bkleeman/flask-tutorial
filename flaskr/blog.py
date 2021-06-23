dce7625b8a77ddc131ecb067d146d73579718092
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-06-23 11:21:07 -0500

    add update views, improve base template, fix create logic

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -28,9 +28,11 @@ def create():
         error = None
 
         if not title:
-            "Title is required."
-        
+            error = 'Title is required.'
+
         if error is not None:
+            flash(error)
+        else:
             db = get_db()
             db.execute(
                 'INSERT INTO post (title, body, author_id)'
@@ -40,7 +42,7 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('/blog/create.html')
+    return render_template('blog/create.html')
 
 def get_post(id, check_author=True):
     post = get_db().execute(
@@ -57,3 +59,39 @@ def get_post(id, check_author=True):
     
     return post
 
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('/blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
